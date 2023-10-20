49b5549f47d182d23b01bc0cccff33504f08bdbc
alimrwn <112070142+alimrwn@users.noreply.github.com>
2023-10-21 04:29:52 +0700

    update welcome blade,css,boostrap
    update terbaru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,127 +109,135 @@
     <div class="cs-height_95 cs-height_lg_70"></div>
     <div class="container">
       <div class="cs-seciton_heading cs-style1 text-center">
-        <div class="cs-section_subtitle wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">Office & Inventory</div>
+        <div class="cs-section_subtitle wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">Kecamatan Sukajadi</div>
         <div class="cs-height_10 cs-height_lg_10"></div>
-        <h3 class="cs-section_title">Our best inventory</h3>
+        <h3 class="cs-section_title">DATA & KEGIATAN</h3>
       </div>
       <div class="cs-height_50 cs-height_lg_40"></div>
-      <style>
-          .my-custom-box {
-           height: 150px; /* Atur tinggi */
-           width: 100%;  /* Atur lebar */
-          /* tambahkan gaya lain sesuai kebutuhan */
-          }
-      </style>
       <div class="row">
         <div class="col-md-3">
   <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
+  <div class="cs-iconbox cs-style1">
     <div class="cs-iconbox_icon cs-center">
       <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
     </div>
     <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
+        <h3 class="cs-iconbox_title" id="tanggalAktif"></h3>
+        <div class="cs-iconbox_subtitle" id="tanggalWaktuAktif"></div>
+    </div>
+
+    <script>
+        function updateTanggalWaktu() {
+            const tanggalAktifElement = document.getElementById("tanggalAktif");
+            const tanggalWaktuAktifElement = document.getElementById("tanggalWaktuAktif");
+            const sekarang = new Date();
+
+            const hari = sekarang.toLocaleDateString('id-ID', { weekday: 'long' });
+            const tanggal = sekarang.toLocaleDateString();
+
+            // Hapus tulisan "Waktu Aktif:" dan spasi sebelum waktu
+            const waktu = sekarang.toLocaleTimeString().replace("Waktu Aktif: ", "");
+
+            tanggalAktifElement.textContent = `${hari}, ${tanggal}`;
+            tanggalWaktuAktifElement.textContent = waktu;
+        }
+
+        // Memanggil fungsi updateTanggalWaktu setiap detik
+        setInterval(updateTanggalWaktu, 1000);
+
+        // Memanggil fungsi untuk menampilkan tanggal dan waktu saat halaman pertama kali dimuat
+        updateTanggalWaktu();
+    </script>
+  </div>
+  <div class="cs-height_25 cs-height_lg_25"></div>
+</div>
+        <div class="col-md-3">
+  <div class="cs-height_25 cs-height_lg_0"></div>
+  <div class="cs-iconbox cs-style1">
+    <div class="cs-iconbox_icon cs-center">
+      <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
+    </div>
+    <div class="cs-iconbox_in">
+      <h3 class="cs-iconbox_title">Layanan</h3>
+      <div class="cs-iconbox_subtitle">Kependudukan</div>
     </div>
   </div>
   <div class="cs-height_25 cs-height_lg_25"></div>
 </div>
         <div class="col-md-3">
   <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
+  <div class="cs-iconbox cs-style1">
     <div class="cs-iconbox_icon cs-center">
       <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
     </div>
     <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
-    </div>
-  </div>
-  <div class="cs-height_25 cs-height_lg_25"></div>
-</div>
-        <div class="col-md-3">
-  <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
-    <div class="cs-iconbox_icon cs-center">
-      <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
-    </div>
-    <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
+      <h3 class="cs-iconbox_title">27.013</h3>
+      <div class="cs-iconbox_subtitle">Rekap Vaksinasi</div>
     </div>
   </div>
   <div class="cs-height_25 cs-height_lg_25"></div>
 </div>
 <div class="col-md-3">
   <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
+  <div class="cs-iconbox cs-style1">
     <div class="cs-iconbox_icon cs-center">
       <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
     </div>
     <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
+      <h3 class="cs-iconbox_title">78</h3>
+      <div class="cs-iconbox_subtitle">Posyandu</div>
     </div>
   </div>
   <div class="cs-height_25 cs-height_lg_25"></div>
 </div>
 <div class="col-md-3">
   <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
+  <div class="cs-iconbox cs-style1">
     <div class="cs-iconbox_icon cs-center">
       <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
     </div>
     <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
+      <h3 class="cs-iconbox_title">Prestasi</h3>
+      <div class="cs-iconbox_subtitle">Tahun Ini</div>
     </div>
   </div>
   <div class="cs-height_25 cs-height_lg_25"></div>
 </div>
 <div class="col-md-3">
   <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
+  <div class="cs-iconbox cs-style1">
     <div class="cs-iconbox_icon cs-center">
       <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
     </div>
     <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
+      <h3 class="cs-iconbox_title">4.878</h3>
+      <div class="cs-iconbox_subtitle">Data</div>
     </div>
   </div>
   <div class="cs-height_25 cs-height_lg_25"></div>
 </div>
 <div class="col-md-3">
   <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
+  <div class="cs-iconbox cs-style1">
     <div class="cs-iconbox_icon cs-center">
       <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
     </div>
     <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
+      <h3 class="cs-iconbox_title">1.192</h3>
+      <div class="cs-iconbox_subtitle">Dutabulin</div>
     </div>
   </div>
   <div class="cs-height_25 cs-height_lg_25"></div>
 </div>
 <div class="col-md-3">
   <div class="cs-height_25 cs-height_lg_0"></div>
-  <div class="cs-iconbox cs-style1 my-custom-box">
+  <div class="cs-iconbox cs-style1">
     <div class="cs-iconbox_icon cs-center">
       <img src="assets/img/icons/icon_box_1.svg" alt="Icon">
     </div>
     <div class="cs-iconbox_in">
-      <div class="cs-iconbox_number cs-primary_font">01</div>
-      <h3 class="cs-iconbox_title">Recipe management & costing</h3>
-      <div class="cs-iconbox_subtitle">Lorem Ipsum is simply dummy text of the printing and formal typesetting</div>
+      <h3 class="cs-iconbox_title">816</h3>
+      <div class="cs-iconbox_subtitle">Rembug Warga</div>
     </div>
   </div>
   <div class="cs-height_25 cs-height_lg_25"></div>
